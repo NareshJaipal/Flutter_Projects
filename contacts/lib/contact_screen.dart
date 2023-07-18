747975da2ef45a15e007bd3b7dea2a1c7772b691
NareshJaipal <132568227+NareshJaipal@users.noreply.github.com>
2023-07-18 05:45:44 -0700

    [+] added bottom bar

---
+++ contacts/lib/contact_screen.dart
@@ -0,0 +1,170 @@
+import 'package:contacts/about_screen.dart';
+// import 'package:contacts/main.dart';
+import 'package:flutter/material.dart';
+
+import 'database.dart';
+
+class ContactScreen extends StatefulWidget {
+  const ContactScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ContactScreen> createState() => _ContactScreenState();
+}
+
+class _ContactScreenState extends State<ContactScreen> {
+  final TextEditingController firstName = TextEditingController();
+  final TextEditingController lastName = TextEditingController();
+  final TextEditingController phoneNo = TextEditingController();
+
+  void updateTextFieldText(BuildContext context) {
+    String updatedName = firstName.text + lastName.text;
+    String updatedNumber = phoneNo.text;
+
+    setState(() {
+      contacts.add({'name': updatedName, 'phoneNumber': updatedNumber});
+    });
+
+    firstName.clear();
+    lastName.clear();
+    phoneNo.clear();
+
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Contacts",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              content: Container(
+                                margin: const EdgeInsets.all(5),
+                                height: 255,
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        IconButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          icon: const Icon(Icons
+                                              .keyboard_arrow_left_rounded),
+                                        ),
+                                        const Text("Create a new contact"),
+                                      ],
+                                    ),
+                                    const Divider(),
+                                    TextField(
+                                      controller: firstName,
+                                      decoration: const InputDecoration(
+                                          label: Text("First name")),
+                                    ),
+                                    TextField(
+                                      controller: lastName,
+                                      decoration: const InputDecoration(
+                                          label: Text("Last name")),
+                                    ),
+                                    TextField(
+                                      controller: phoneNo,
+                                      decoration: const InputDecoration(
+                                          label: Text("Phone")),
+                                    ),
+                                    const SizedBox(height: 15),
+                                    TextButton(
+                                      child: const Text("Save"),
+                                      onPressed: () =>
+                                          updateTextFieldText(context),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                      child: const Text("Add Contact"),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[50],
+                    hintText: 'Search name, number, etc',
+                    prefixIcon: const Icon(Icons.search_rounded),
+                    hoverColor: Colors.lightBlueAccent,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: contacts.length,
+              itemBuilder: (BuildContext context, int index) {
+                final sortedContacts = [...contacts];
+                sortedContacts.sort((a, b) => a['name'].compareTo(b['name']));
+
+                final contact = sortedContacts[index];
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: ListTile(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => AboutScreen(
+                                name: contact['name'],
+                                phoneNumber: contact['phoneNumber'],
+                              ),
+                            ),
+                          );
+                        },
+                        leading: const CircleAvatar(
+                            backgroundColor: Colors.lightBlueAccent,
+                            child: Icon(Icons.person)),
+                        title: Text(contact['name']),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(left: 25, right: 25),
+                      child: const Divider(),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- contacts/lib/home_screen.dart
+++ contacts/lib/home_screen.dart
@@ -1,8 +1,6 @@
-import 'package:contacts/about_screen.dart';
-// import 'package:contacts/main.dart';
 import 'package:flutter/material.dart';
 
-import 'database.dart';
+import 'contact_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,158 +10,46 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  
-  final TextEditingController firstName = TextEditingController();
-  final TextEditingController lastName = TextEditingController();
-  final TextEditingController phoneNo = TextEditingController();
+  int _selectedIndex = 0;
 
-  void updateTextFieldText(BuildContext context) {
-    String updatedName = firstName.text + lastName.text;
-    String updatedNumber = phoneNo.text;
+  static const List<Widget> _contactAppOption = <Widget>[
+    Text('Recent Page'),
+    ContactScreen(),
+    Text('Favorite Page'),
+  ];
 
+  void _onItemTapped(int index) {
     setState(() {
-      contacts.add({'name': updatedName, 'phoneNumber': updatedNumber});
+      _selectedIndex = index;
     });
-
-    firstName.clear();
-    lastName.clear();
-    phoneNo.clear();
-
-    Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                const SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "Contacts",
-                      style: TextStyle(
-                        fontSize: 28,
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        createNewAccount(context);
-                      },
-                      child: const Text("Add Contact"),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[50],
-                    hintText: 'Search name, number, etc',
-                    hoverColor: Colors.lightBlueAccent,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                )
-              ],
-            ),
+      body: Center(
+        child: _contactAppOption.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time_rounded),
+            label: 'Recent',
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: contacts.length,
-              itemBuilder: (BuildContext context, int index) {
-                final sortedContacts = [...contacts];
-                sortedContacts.sort((a, b) => a['name'].compareTo(b['name']));
-
-                final contact = sortedContacts[index];
-                return Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(4),
-                      child: ListTile(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => AboutScreen(
-                                name: contact['name'],
-                                phoneNumber: contact['phoneNumber'],
-                              ),
-                            ),
-                          );
-                        },
-                        leading: const CircleAvatar(
-                            backgroundColor: Colors.lightBlueAccent,
-                            child: Icon(Icons.person)),
-                        title: Text(contact['name']),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(left: 25, right: 25),
-                      child: const Divider(),
-                    ),
-                  ],
-                );
-              },
-            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Contacts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star_border_outlined),
+            label: 'Favorite',
           ),
         ],
+        onTap: _onItemTapped,
       ),
-    );
-  }
-
-  Future<dynamic> createNewAccount(BuildContext context) {
-    return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          content: Container(
-            margin: const EdgeInsets.all(5),
-            height: 255,
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      icon: const Icon(Icons.keyboard_arrow_left_rounded),
-                    ),
-                    const Text("Create a new contact"),
-                  ],
-                ),
-                const Divider(),
-                TextField(
-                  controller: firstName,
-                  decoration: const InputDecoration(label: Text("First name")),
-                ),
-                TextField(
-                  controller: lastName,
-                  decoration: const InputDecoration(label: Text("Last name")),
-                ),
-                TextField(
-                  controller: phoneNo,
-                  decoration: const InputDecoration(label: Text("Phone")),
-                ),
-                const SizedBox(height: 15),
-                TextButton(
-                  child: const Text("Save"),
-                  onPressed: () => updateTextFieldText(context),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
     );
   }
 }
